de3f474ed195e62cde0bc38e4415c74fb08812a2
shongrin <shongrin@gmail.com>
2022-07-18 21:17:20 +0300

    Fix added the actual project

--- PetShop.Client/ModelsViews/AnimalViewModel.cs
+++ PetShop.Client/ModelsViews/AnimalViewModel.cs
@@ -5,24 +5,24 @@ namespace PetShop.Client.Models
 {
     public class AnimalViewModel
     {
-        [Display(Name = "Animal Id:")]
+        [Display(Name = "Id")]
         public int AnimalId { get; set; }
-        [Display(Name = "Animal Name:")]
+        [Display(Name = "Name")]
         [Required(ErrorMessage = "Please Enter a name")]
         public string Name { get; set; } = null!;
-        [Display(Name = "Description:")]
+        [Display(Name = "Description")]
         [DataType(DataType.MultilineText)]
         [Required(ErrorMessage = "Please Enter a description")]
         [StringLength(30, ErrorMessage = "Only 30 letters allowed")]
         public string? Description { get; set; }
-        [Display(Name = "BirthDate:")]
+        [Display(Name = "BirthDate")]
         [DataType(DataType.DateTime)]
         public DateTime? BirthDate { get; set; }
-        [Display(Name = "Protate:")]
+        [Display(Name = "Protate")]
         [DataType(DataType.Upload)]
         public IFormFile? Photo { get; set; }
         public string? PhotoUrl { get; set; }
-        [Display(Name = "Category Name:")]
+        [Display(Name = "Category Name")]
         [Required(ErrorMessage = "Please Enter a Category")]
         public int? CategoryId { get; set; }
         [Display(Name = "Category")]
